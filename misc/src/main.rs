891bbc4abe70dbb8cb6b842923188e2bb8b30712
Kevin Phung <phung.kevin@gmail.com>
2018-09-01 03:38:44 -0600

    generics

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -41,63 +41,131 @@
 //     println!("{}", ttt);
 // }
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut scores = HashMap::new();
+
+//     scores.insert(String::from("Blue"), 10);
+//     scores.insert(String::from("Yellow"), 50);
+
+//     println!("{:#?}", scores);
+
+//     ///////////////////////
+
+//     let teams = vec![String::from("Green"), String::from("Red")];
+//     let initial_scores = vec![33, 55];
+
+//     let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+//     println!("{:#?}", scores);
+
+//     let team_name = String::from("Red");
+//     let score = scores.get(&team_name);
+//     match score {
+//         None => (),
+//         Some(i) => println!("{} score is {}", team_name, i)
+//     };
+
+//     // unwrapped is discouraged because it can be None
+//     // The preferred method of handling is to use match like above
+//     let sc = score.unwrap();
+//     println!("unwrapped score : {}", sc);
+
+//     ////////
+//     let mut scores = HashMap::new();
+
+//     scores.insert(String::from("Avengers"), 55);
+//     scores.insert(String::from("Justice League"), 10);
+
+//     for (key, value) in &scores {
+//         println!("{}: {}", key, value);
+//     }
+
+//     scores.insert(String::from("Avengers"), 100);
+//     println!("{:#?}", scores);
+
+//     scores.entry(String::from("Justice League")).or_insert(30);
+//     scores.entry(String::from("X-Men")).or_insert(70);
+
+//     println!("{:#?}", scores);
+
+//     //////////////
+
+//     let text = "hello world wonderful world";
+//     let mut map = HashMap::new();
+
+//     for word in text.split_whitespace() {
+//         let count = map.entry(word).or_insert(0);
+//         *count += 1;
+//     }
+//     println!("{:?}", map);
+// }
+
 
 fn main() {
-    let mut scores = HashMap::new();
+    let number_list = vec![34, 50, 25, 100, 65];
+    // let mut largest = number_list[0];
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    // for number in number_list {
+    //     if number > largest {
+    //         largest = number;
+    //     }
+    // }
+    // println!("The largest number is {}", largest);
 
-    println!("{:#?}", scores);
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
 
-    ///////////////////////
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
 
-    let teams = vec![String::from("Green"), String::from("Red")];
-    let initial_scores = vec![33, 55];
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    println!("{:#?}", scores);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
-    let team_name = String::from("Red");
-    let score = scores.get(&team_name);
-    match score {
-        None => (),
-        Some(i) => println!("{} score is {}", team_name, i)
-    };
+}
 
-    // unwrapped is discouraged because it can be None
-    // The preferred method of handling is to use match like above
-    let sc = score.unwrap();
-    println!("unwrapped score : {}", sc);
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    ////////
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Avengers"), 55);
-    scores.insert(String::from("Justice League"), 10);
-
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    scores.insert(String::from("Avengers"), 100);
-    println!("{:#?}", scores);
+    largest
+}
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
 
-    scores.entry(String::from("Justice League")).or_insert(30);
-    scores.entry(String::from("X-Men")).or_insert(70);
-
-    println!("{:#?}", scores);
-
-    //////////////
-
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
-    println!("{:?}", map);
+
+    largest
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
